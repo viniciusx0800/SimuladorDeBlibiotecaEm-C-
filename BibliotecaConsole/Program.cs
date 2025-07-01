96f04926dff909941984c3ae34e2af084403c73e
Viniciusx0800 <vinioliveira2008y@gmail.com>
2025-07-01 15:49:03 -0300

    feat : iniciar uma proposta de menu escolha

--- BibliotecaConsole/Program.cs
+++ BibliotecaConsole/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+
+
+Console.WriteLine("============== Simulador de Biblioteca ==============");
+Console.WriteLine("1. Cadastrar Usuario");
+Console.WriteLine("2. Cadastrar Livro");
+Console.WriteLine("3. Listar Livros Disponíveis para Empréstimo");  
+Console.WriteLine("4. Emprestar Livro");
+Console.WriteLine("5. Devolver Livro");
+Console.WriteLine("6. Listar Empréstimos Ativos");
+Console.WriteLine("7. Histórico de Empréstimos por Usuário");
+Console.WriteLine("0. Sair");
+Console.WriteLine("======================================================");
+Console.Write("Escolha uma opção: ");
